b5d56bfefbbdc6f39075fbf83fb56e3577bb472d
Jacek Tomaszewski <jacek@jtom.me>
2020-07-08 15:24:04 +0200

    gitpkg

---
+++ lib/typescript/src/index.d.ts
@@ -0,0 +1,13 @@
+export { default as TabBar } from './TabBar';
+export type { Props as TabBarProps } from './TabBar';
+export { default as TabView } from './TabView';
+export type { Props as TabViewProps } from './TabView';
+export { default as TabBarIndicator } from './TabBarIndicator';
+export type { Props as TabBarIndicatorProps } from './TabBarIndicator';
+export type { Props as TabBarItemProps } from './TabBarItem';
+export { default as TouchableItem } from './TouchableItem';
+export { default as SceneMap } from './SceneMap';
+export { default as Pager } from './Pager';
+export type { Props as PagerProps } from './Pager';
+export { default as ScrollPager } from './ScrollPager';
+export type { Route, NavigationState, SceneRendererProps } from './types';
